109a930bfda80a991220c9a66fbb736e6129c678
Remigiusz Dudek <remigiusz_dudek@tlen.pl>
2016-11-26 20:46:26 +0100

    poprawione klasy

--- src/TriangleValidator.java
+++ src/TriangleValidator.java
@@ -1,47 +1,9 @@
 
 public class TriangleValidator {
-	private double a;
-	private double b;
-	private double c;
-
-	public TriangleValidator(int a, int b, int c) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public double getA() {
-		return a;
-	}
-
-	public void setA(double a) {
-		this.a = a;
-	}
-
-	public double getB() {
-		return b;
-	}
-
-	public void setB(double b) {
-		this.b = b;
-	}
-
-	public double getC() {
-		return c;
-	}
-
-	public void setC(double c) {
-		this.c = c;
-	}
-
-	public boolean validate(double a, double b, double c) {
-		if (a + b > c) {
-			if (a + c > b) {
-				if (b + c > a) {
-					return true;
-				}
-			}
-		}
-		return false;
-
-	}
+    public boolean validate(double a, double b, double c) {
+        return a + b > c
+                && a + c > b
+                && b + c > a;
+    }
 }
 
